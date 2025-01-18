cae3fc7a4eaf38ea0c964310167aeb7a28f59104
Mohaimin Topu <abmtopu73@gmail.com>
2025-01-18 19:08:15 +0600

    tuples basic

---
+++ tuples.py
@@ -0,0 +1,7 @@
+# Tuple is a built in data type that let's us create immutable sequences of values
+
+myTuple = (1, 2, 3, 4, 5, 5, 6, 6, 7,)
+
+myUpdatedTuple = myTuple.count(5)
+
+print(myUpdatedTuple)
